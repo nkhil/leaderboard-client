df23ed331e39699345cf64482fb20c1979315017
Nikhil Vijayan <nikhil.vijayan@bglgroup.co.uk>
2021-02-15 12:22:00 +0000

    feat: :sparkles: Add get token functionality
    Add a get token functionality by initialising the package with client credentials (clientId, clientSecret and apiKey)

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const client = axios.create({
   timeout: 1000,
 });
 
-const setAuthToken = token => {
-  if (token) {
-    //applying token
-    client.defaults.headers.common['Authorization'] = token;
-  } else {
-    //deleting the token from header
-    delete client.defaults.headers.common['Authorization'];
-  }
-}
+// const setAuthToken = token => {
+//   if (token) {
+//     //applying token
+//     client.defaults.headers.common['Authorization'] = token;
+//   } else {
+//     //deleting the token from header
+//     delete client.defaults.headers.common['Authorization'];
+//   }
+// }
 
-async function liveness() {
+async function liveness({ clientId, clientSecret, apiKey }) {
   try {
     const response = await client.get('/liveness')
     return response.data
@@ -24,10 +24,39 @@ async function liveness() {
   }
 }
 
-function exportedFn({ clientId, clientSecret, apiKey }) {
-  return {
-    getLiveness: liveness,
+function checkConfig(config) {
+  const { clientId, clientSecret, apiKey } = config;
+  if (!clientId) {
+    throw new Error('Initialisation error: clientId not provided')
+  } else if (!clientSecret) {
+    throw new Error('Initialisation error: clientSecret not provided')
+  } else if (!apiKey) {
+    throw new Error('API key not provided')
+  }
+  return true
+}
+
+async function getToken({ clientId, clientSecret, apiKey }) {
+  try {
+    const response = await client.get('/token', {
+      headers: {
+        'x-client-id': clientId,
+        'x-client-secret': clientSecret,
+        'x-api-key': apiKey,
+      }
+    })
+    return response.data
+  } catch (error) {
+    console.log('🚀 ~ file: index.js ~ line 43 ~ getToken ~ error', error)
   }
 }
 
-module.exports = exportedFn
+function sdk(config) {
+  checkConfig(config)
+  return {
+    getLiveness: () => liveness(config),
+    getToken: () => getToken(config)
+  }
+}
+
+module.exports = sdk
